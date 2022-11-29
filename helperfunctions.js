37c3ec7d0b7d66601cbdbe9d3861ad6c20eb849f
unknown <3687036+JackC27@users.noreply.github.com>
2022-11-29 01:41:05 -0500

    just make it work

--- helperfunctions.js
+++ helperfunctions.js
@@ -1,17 +1,21 @@
-export function arrayOfRandomCharaters(strLen = 20, arrLen = 10, useSpecialChar=false) {
-  const special = "!@#$%^&*()_+";
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  if(useSpecialChar){
-    alphabet.concat(special);
-  }
-  let str = "";
-  while (str.length < strLen) {
-    str = str + alphabet[Math.floor(Math.random() * alphabet.length)]
-  }
-  let x = [];
-  while (x.length < arrLen) {
-    x.push(generateRandomLetter())
+function randChar(strLen = 20, arrLen = 10, useSpecialChar=false){
+  function arrayOfRandomCharaters() {
+    const special = "!@#$%^&*()_+";
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let str = "";
+    while (str.length < strLen) {
+      str = str + alphabet[Math.floor(Math.random() * alphabet.length)];
+    }
+    return str;
   }
 
-  return str;
+  let randStrings = [];
+  while (randStrings.length < arrLen) {
+    randStrings.push(arrayOfRandomCharaters())
+  }
+  return randStrings;
 }
+
+let x = randChar(20, 5);
+
+console.log("X: ", x);
